44392fce388dc72deedcfe0fcbf285ae6d7c4f7a
Oliver Servín <oliverds@users.noreply.github.com>
2022-02-02 09:53:29 -0600

    Remove footer

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -67,18 +67,5 @@
         <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
 
         @stack('scripts')
-
-        <footer class="bg-white text-center text-sm mt-12 py-4" role="contentinfo">
-            <ul class="flex flex-col md:flex-row justify-center">
-                <li class="md:mr-2">
-                    &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
-                </li>
-
-                <li>
-                    Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>
-                    and <a href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
-                </li>
-            </ul>
-        </footer>
     </body>
 </html>
